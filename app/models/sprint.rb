611ea46858e737a7cee210c064776e9adfcd1a00
Jordan Hatch <jordan@jordanh.net>
2021-08-09 22:13:09 +1000

    Add paper trail and validations to sprints

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,11 +1,15 @@
 class Sprint < ApplicationRecord
   has_many :updates
+  has_paper_trail skip: [:created_at, :updated_at]
 
   scope :in_date_order, ->{ order('start_on asc') }
+  scope :in_reverse_date_order, -> { order('start_on desc') }
   scope :recent, -> { where('start_on < ?', Date.today).order('start_on desc') }
 
   scope :previous_sprints, ->(sprint) { where('start_on < ?', sprint.start_on).order('start_on DESC') }
   scope :next_sprints, ->(sprint) { where('start_on > ?', sprint.start_on).order('start_on ASC') }
+
+  validates :name, :start_on, :end_on, presence: true
 
   def previous_sprint
     Sprint.previous_sprints(self).first
