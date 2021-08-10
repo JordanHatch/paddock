d31e2d498a719e82f86faefda7a127261f600eb1
Jordan Hatch <jordan@jordanh.net>
2021-08-10 18:14:25 +1000

    Only show the 'manage' tag to admin users

--- spec/features/manage_spec.rb
+++ spec/features/manage_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe 'the manage section', type: :feature do
 
   describe 'accessing the section' do
     context 'as an admin user', admin_user: true do
+      it "displays a 'Manage' tag" do
+        visit root_path
+        within 'header.app' do
+          expect(page).to have_link('Manage')
+        end
+      end
+
       it 'renders the page' do
         visit manage_root_path
         expect(page).to have_content('Manage')
@@ -11,6 +18,13 @@ RSpec.describe 'the manage section', type: :feature do
     end
 
     context 'as a regular user', admin_user: false do
+      it "doesn't display a 'Manage' tag" do
+        visit root_path
+        within 'header.app' do
+          expect(page).to_not have_link('Manage')
+        end
+      end
+
       it 'returns a 404 error' do
         visit manage_root_path
         expect(page).to have_http_status(404)
