6780c6ab70cd0804dc9aa0e5f63632274daff979
Jordan Hatch <jordan@jordanh.net>
2021-08-14 16:44:53 +1000

    Show the environment in the <title> tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,12 @@ module ApplicationHelper
       link_to label, link
     end
   end
+
+  def paddock_env
+    ENV['PADDOCK_ENV']
+  end
+
+  def display_paddock_env?
+    paddock_env.present? && paddock_env != 'production'
+  end
 end
