84b02c0def4785a45839b4481da5b979e1e70d34
Jordan Hatch <jordan@jordanh.net>
2021-08-21 09:07:37 +1000

    Show a user icon when redeeming invitations

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,2 +1,13 @@
 module InvitationsHelper
+
+  def invitation_user_initials(email)
+    mailbox = email.split('@').first
+
+    initials = mailbox.split('.').map {
+      |string| string[0]
+    }
+
+    initials.join[0,2].upcase
+  end
+
 end
---
+++ spec/helpers/invitations_helper_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe InvitationsHelper, type: :helper do
+
+  describe '#invitation_user_initials' do
+    subject { helper.invitation_user_initials(email) }
+
+    context 'with a firstname.lastname email address' do
+      let(:email) { 'test.user@example.org' }
+
+      it 'returns the initials' do
+        expect(subject).to eq('TU')
+      end
+    end
+
+    context 'with more than two dots in the email' do
+      let(:email) { 'test.user.email@example.org' }
+
+      it 'returns only the first two initials' do
+        expect(subject).to eq('TU')
+      end
+    end
+
+    context 'with no dots in the email' do
+      let(:email) { 'test@example.org' }
+
+      it 'returns only the first initial' do
+        expect(subject).to eq('T')
+      end
+    end
+  end
+
+end
