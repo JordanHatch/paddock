eebb54369b857ed4d5ac09564e6cb4971bba062f
Jordan Hatch <jordan@jordanh.net>
2021-09-01 22:34:50 +1000

    Extract the section header component

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -10,11 +10,11 @@ module NavigationHelper
   def section_nav_link(label, path, section_name)
     nav_link(label,
              path,
-             css_class: "section-#{section_name}",
+             list_item_class: "section-#{section_name}",
              match_prefix: true)
   end
 
-  def nav_link(label, path, css_class: nil, match_prefix: false)
+  def nav_link(label, path, list_item_class: nil, link_class: nil, match_prefix: false)
     if match_prefix
       prefix_matches = request.fullpath =~ /^#{Regexp.escape(path)}/
       selected = current_page?(path) || prefix_matches
@@ -22,11 +22,11 @@ module NavigationHelper
       selected = current_page?(path)
     end
 
-    styles = [css_class]
+    styles = [list_item_class]
     styles << 'selected' if selected
 
     content_tag(:li, class: styles.compact.join(' ')) do
-      link_to label, path
+      link_to label, path, class: link_class
     end
   end
 end
