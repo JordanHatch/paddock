177a76faaaa8424758e25fb0b45ec6c5eea0a9c4
Jordan Hatch <jordan@jordanh.net>
2021-09-05 14:25:08 +1000

    Refactor form-actions component

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -88,7 +88,7 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'button'
+  config.button_class = 'button button--primary'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
