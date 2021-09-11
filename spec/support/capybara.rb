e72c0dff2b18042faa935c4e61e125c0df90f493
Jordan Hatch <jordan@jordanh.net>
2021-09-11 16:24:59 +1000

    Increase Ferrum timeout
    This should reduce the likelihood of flaky tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,10 +2,10 @@ require 'capybara/cuprite'
 
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, headless: true)
+  Capybara::Cuprite::Driver.new(app, headless: true, timeout: 10)
 end
 
-Capybara.default_max_wait_time = 5
+Capybara.default_max_wait_time = 10
 
 # This allows Capybara to interact with our custom checkbox and radio button
 # components, where the <input> is hidden and the <label> tag includes the
