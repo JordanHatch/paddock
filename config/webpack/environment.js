4604688e3a2b40f81316b6fe897a967da9b0a507
Jordan Hatch <jordan@jordanh.net>
2021-09-11 14:06:23 +1000

    Refactor colours
    Simplify colour variables (and add new grey and red palettes)
    Add a new helper for accessing colours, and update all references.
    Remove old colour variables.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,12 @@ environment.loaders.get('sass')
   .use
   .find(item => item.loader === 'sass-loader')
   .options
-  .sassOptions = {importer: globImporter()};
+  .sassOptions = {
+    importer: globImporter(),
+    includePaths: [
+      'app/assets/stylesheets'
+    ]
+  }
 
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
