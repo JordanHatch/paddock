51ed270c5ccdf2661f95480cb2cfa34d4e9f1994
Jordan Hatch <jordan@jordanh.net>
2022-01-19 18:43:55 +1100

    Enable paper_trail for commitments

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -15,8 +15,10 @@ class Commitment < ApplicationRecord
   enumerize :commodities, in: COMMODITIES, multiple: true
 
   belongs_to :quarter
-
   has_many :team_commitments
   has_many :teams, through: :team_commitments
+
+  has_paper_trail skip: %i[created_at updated_at]
+
   scope :in_order, -> { order('number ASC') }
 end
