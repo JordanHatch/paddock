1f222df868aa0d013312a72b083995ae21fef9ef
Jordan Hatch <jordan@jordanh.net>
2022-01-26 17:36:39 +1100

    Further asset fixes and tidying up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_options = {
+    skip_premailer: true
+  }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
