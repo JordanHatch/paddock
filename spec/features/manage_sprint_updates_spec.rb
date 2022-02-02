d298e4d8245b925bd52c4bdf8f84ebe949cebd98
Jordan Hatch <jordan@jordanh.net>
2022-02-02 20:47:38 +1100

    Turn the sprint update danger zone into a modal

--- spec/features/manage_sprint_updates_spec.rb
+++ spec/features/manage_sprint_updates_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'managing sprint updates', type: :feature, admin_user: true do
   describe 'unpublishing updates' do
-    it 'can unpublish a sprint update' do
-      success_message = I18n.t(:success, scope: %w[services sprint_updates unpublish])
-
+    it 'can unpublish a sprint update', js: true do
       update = create(:published_sprint_update)
       sprint = update.sprint
       team = update.team
@@ -13,8 +11,6 @@ RSpec.describe 'managing sprint updates', type: :feature, admin_user: true do
       click_on team.name
       click_on 'Danger zone'
       click_on 'Unpublish'
-
-      expect(page).to have_content(success_message)
 
       visit sprint_path(sprint)
       click_on team.name
