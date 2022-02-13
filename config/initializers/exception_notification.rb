36f0b903fed02853881a77ff00a45e4d97e0b2ab
Jordan Hatch <jordan@jordanh.net>
2022-02-13 15:17:29 +1100

    Set up the exception_notification gem

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,15 @@
+if ENV['EXCEPTION_NOTIFIER_ACTIVE'].present?
+  paddock_env = ENV.fetch('PADDOCK_ENV', 'None')
+  from = ENV.fetch('EXCEPTION_NOTIFIER_FROM', 'paddock@localhost')
+  to = ENV.fetch('EXCEPTION_NOTIFIER_TO', '').split(',')
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+      email_prefix: "[Paddock (#{paddock_env})] ",
+      sender_address: %{"Paddock Exceptions" <#{from}>},
+      exception_recipients: to,
+      email_format: :html,
+      sections: %w{request backtrace},
+    },
+    error_grouping: true
+end
